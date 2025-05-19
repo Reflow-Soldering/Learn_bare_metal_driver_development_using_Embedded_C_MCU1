8465a80350aee2be10f41cdc217b0a6c680148bb
Reflow-Soldering <reflow_soldering@naver.com>
2025-05-19 22:55:59 +0900

    2025/5/19
    API 프로토타입 작성

---
+++ stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h
@@ -0,0 +1,78 @@
+/*
+ * stm32f407xx_gpio_driver.h
+ *
+ *  Created on: May 19, 2025
+ *      Author: user
+ */
+
+#ifndef INC_STM32F407XX_GPIO_DRIVER_H_
+#define INC_STM32F407XX_GPIO_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+typedef struct
+{
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+	uint8_t GPIO_AltFunMode;
+
+}GPIO_PinConfig_t;
+
+/*
+ * GPIO pin handle structure
+ */
+
+typedef struct
+{
+	// GPIO 장치들의 기본값을 포인터로 지정
+	GPIO_RegDef_t *pGPIOx;
+	GPIO_PinConfig_t GPIO_PinConfig;
+
+}GPIO_Handle_t;
+
+/**********************************************************
+ * API
+ */
+
+
+/*
+ * GPIO CLK Set
+ * GPIO 클럭 연결 여부
+ */
+
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
+
+/*
+ * GPIO Init-Deinit
+ * GPIO
+ */
+void GPIO_Init(GPIO_Handle_t *pGPIOHandles);
+
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);
+
+/*
+ * Data read/write
+ */
+
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx);
+
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value);
+
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value);
+
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber);
+
+
+/*
+ * IRQ Configuration
+ */
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi);
+
+void GPIO_IRQHandling(uint8_t PinNumber);
+
+#endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
---
+++ stm32f4xx_drivers/drivers/src/stm32f407xx_gpio_driver.c
@@ -0,0 +1,9 @@
+/*
+ * stm32f407xx_gpio_driver.c
+ *
+ *  Created on: May 19, 2025
+ *      Author: user
+ */
+
+
+#include "stm32f407xx_gpio_driver.h"
